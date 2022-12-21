c17245bd0497eb6e8639e9cd3059091038ea8a39
Rodolfo Andino <rodolfoandino@Rodolfos-MacBook-Pro.local>
2022-12-20 21:18:25 -0600

    feat(presentation): added first component & code clean up

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import presentation from "./components/Presentation.vue"
 </script>
 
 <template>
-  <h1>Rodolfo Andino</h1>
-  <h3>Web Developer</h3>
+  <presentation/>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/Presentation.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+
+</script>
+
+<template>
+  <div class="h-screen grid place-content-center w-screen">
+    <h1>Rodolfo Andino</h1>
+    <h3>Web Developer</h3>
+  </div>
+</template>
